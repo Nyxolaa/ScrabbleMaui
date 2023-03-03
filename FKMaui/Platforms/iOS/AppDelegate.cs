086de91b082c089f19200b6e412efee7f9bb298e
Nyxola <88090162+Nyxolaa@users.noreply.github.com>
2023-03-03 22:53:23 +0300

    Fundamentals

---
+++ FKMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace FKMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
